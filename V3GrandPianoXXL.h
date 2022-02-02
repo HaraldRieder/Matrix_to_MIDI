81fb64d3ad7ff3701ca0696cc0180980edaa34bc
HaraldRieder <Harald.Rieder@online.de>
2022-02-02 08:03:53 +0100

    entered more V3 sound settings

--- V3GrandPianoXXL.h
+++ V3GrandPianoXXL.h
@@ -4,17 +4,63 @@ struct PresetSound {
 };
 
 const struct PresetSound 
-  GrandViennaSoft = { 0, 6 },
-  GrandPianoHamburg = { 0, 1 },
-  RhodesMK1Soft = { 0, 36 },
-  JazzBass = { 3, 67 },
-  BassUS1 = { 3, 79 },
+  GrandPianoVienna = { 0, 4 }, // Bösendorfer
+  GrandPianoHamburg = { 0, 5 }, // Steinway
+  GPViennaLayeredPad = { 0, 16 },
+  GPHamburgLayeredPad = { 0, 17 },
+  GPViennaLayeredStrings = { 0, 18 },
+  GPHamburgLayeredStrings = { 0, 19 },
+  MK1DynoTremolo = { 0, 31 }, // Fender Rhodes
+  MK1DynoLayeredPad = { 0, 32 },
+  MK1Tremolo = { 0, 37 },
+  MK1LayeredFM = { 0, 39 },
+  A200Tremolo1 = { 0, 41 }, // Wurlitzer
+  FMPianoLayeredMKS = { 0, 51 },
+  V3BellaLayeredCortales = { 0, 54 },
+  Harpsichord = { 3, 47 },
+  HarpsichordOctave = { 3, 48 },
+  GuitarNylon = { 3, 22 },
+  GuitarNylonSoft = { 3, 23 },
+  GuitarSteel = { 3, 26 },
+  GuitarSteelSoft = { 3, 27 },
+  HarpLong = { 3, 60 },
+  UprightJazzBass = { 3, 71 }, 
+  EBassUS1 = { 3, 79 },
+  EBassUS1 = { 3, 80 },
+  EBassPick = { 3, 82 },
+  EBassPickDark = { 3, 83 },
+  EBassFretless = { 3, 84 },
+  SlapBass1 = { 3, 85 },
+  SlapBass2 = { 3, 86 },
+  SynBass = { 2, 116 },
+  JBassSoft = { 3, 121 },
+  CSClassicBass = { 3, 122 },
+  MOBassENV = { 3, 124 },
+  XBass1 = { 3, 125 },
+  XBass2 = { 3, 126 },
+  PopDrumKit = { 4, 120 },
+  JazzDrumKit = { 4, 121 },
+  OrchestraPercussion = { 4, 122 },
+  VoiceKit = { 4, 123 },
   NoSound = { 0, 127 };
 
+// my favourite non-split sounds
 const PresetSound preset_sounds[] = {
-  GrandViennaSoft,
-  GrandPianoHamburg,
-  RhodesMK1Soft
+  GrandPianoHamburg, GrandPianoVienna, 
+  GPViennaLayeredPad, GPHamburgLayeredPad,
+  GPHamburgLayeredStrings, GPViennaLayeredStrings,
+  Harpsichord, HarpsichordOctave,
+  MK1DynoTremolo, MK1DynoLayeredPad,
+  MK1Tremolo, MK1LayeredFM,
+  A200Tremolo1, FMPianoLayeredMKS,
+  V3BellaLayeredCortales, NoSound, // reserved
+  GuitarNylon, GuitarNylonSoft,
+  GuitarSteel, GuitarSteelSoft,
+  HarpLong, NoSound, // reserved
+  PopDrumKit = { 4, 120 },
+  JazzDrumKit = { 4, 121 },
+  OrchestraPercussion = { 4, 122 },
+  VoiceKit = { 4, 123 }
 };
 
 const int n_preset_sounds = sizeof(preset_sounds) / sizeof (preset_sounds[0]);
